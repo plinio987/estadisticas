8e9abd344012a8723c9568853cc5b59282f92e7b
plinio <plinio987@gmail.com>
2020-06-29 12:22:34 -0400

    first commit

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 
    Copyright &copy; 2017-2019 Creado por
-   <strong>Gabinete Digital</strong> &nbsp &nbsp &nbsp Reportar inconvenientes a: &nbsp   <a href="mailto:plinio.polanco@vicepresidencia.gob.do">Plinio Polanco</a>
+   <strong>Gabinete Digital</strong> &nbsp &nbsp &nbsp Reportar inconvenientes a &nbsp   <a href="mailto:plinio.polanco@vicepresidencia.gob.do">Plinio Polanco</a>
     <div class="float-right d-none d-sm-inline-block">
     <a href="faq.php">FAQ</a> &nbsp &nbsp &nbsp &nbsp  <b>Version</b> 3.0
     </div>
